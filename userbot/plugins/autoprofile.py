d00b6e120ee05360a71295bf70e4f8cfcd070dee
premettan <48172305+premettan@users.noreply.github.com>
2020-06-05 11:47:43 +0530

    Update autoprofile.py

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -19,7 +19,7 @@ from pySmartDL import SmartDL
 import shutil
 
 
-DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ  "
+DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " EVERYBODY GOSES TO THE SAME PLACE WHEN DIE  "
 
 DEL_TIME_OUT = 30
 DEFAULTUSER = str(AUTONAME) if AUTONAME else "✰Sᴀͥʀᴀͣᴛͫʜ™✰" 
@@ -182,7 +182,8 @@ async def _(event):
         Bio = BIO_STRINGS[bro]
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        #bio = f"📅 {DMY} | ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ | ⌚️ {HM}"
+DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " EVERYBODY GOSES TO THE SAME PLACE WHEN DIE  "
+        #bio = f"📅 {DMY} |  | ⌚️ {HM}"
         logger.info(Bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
