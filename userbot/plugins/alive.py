38062c4882b695fc9463bb663de08a9a85ab9069
premettan <48172305+premettan@users.noreply.github.com>
2020-06-13 23:43:41 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,14 +11,14 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+    await alive.edit("**SHINIGAMI IS RUNNING SUCCESFULLY**\n\n"
                      "`☞Telethon version: 1.11.3\n`"
                      "`☞Python: 3.8.2\n`"
-                     "`☞Bot was modified by:` @darthvade_r\n"
+                     "`☞Bot was modified by:`@DEATH_LORD_MICTLANTECUHTLI\n"
                      "`☞and created by :` snapdragon,anubis\n"
                      "`☞Database Status: Databases functioning normally!\n\n`"
                      "`☞Always with you, my master!\n`"
-                     f"`☞Owner Name`: [{DEFAULTUSER}](t.me/darthvade_r)\n"
+                     f"`☞Owner Name`: [{DEFAULTUSER}](t.me/@DEATH_LORD_MICTLANTECUHTLI)\n"
                      "☞[Deploy this userbot Now](https://github.com/Sur-vivor/CatUserbot)"
                     )
 
